7396982458bff6843bbf7a9cef25f9260a7c4905
wbeyda <warrenbeyda@gmail.com>
2024-07-10 16:18:35 -0600

    Q3 partial answer. No unittests. But view seems to be correct

--- interview/order/urls.py
+++ interview/order/urls.py
@@ -1,10 +1,10 @@
 
 from django.urls import path
-from interview.order.views import OrderListCreateView, OrderTagListCreateView
+from interview.order.views import OrderListCreateView, OrderTagListCreateView, OrderListByDateRangeView
 
 
 urlpatterns = [
     path('tags/', OrderTagListCreateView.as_view(), name='order-detail'),
     path('', OrderListCreateView.as_view(), name='order-list'),
-
+    path('by-date/', OrderListByDateRangeView.as_view(), name='order-list-by-date-range'),
 ]
--- interview/order/views.py
+++ interview/order/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from datetime import datetime, timedelta
+
 from rest_framework import generics
 
 from interview.order.models import Order, OrderTag
@@ -13,3 +14,23 @@ class OrderListCreateView(generics.ListCreateAPIView):
 class OrderTagListCreateView(generics.ListCreateAPIView):
     queryset = OrderTag.objects.all()
     serializer_class = OrderTagSerializer
+
+
+class OrderListByDateRangeView(generics.ListAPIView):
+    serializer_class = OrderSerializer
+    
+    def get_queryset(self):
+        start_date_str = self.request.query_params.get('start_date', None)
+        embargo_date_str = self.request.query_params.get('embargo_date', None)
+        
+        if not start_date_str or not embargo_date_str:
+            return Order.objects.none()
+        
+        try:
+            start_date = datetime.strptime(start_date_str, '%Y-%m-%d').date()
+            embargo_date = datetime.strptime(embargo_date_str, '%Y-%m-%d').date()
+        except ValueError:
+            return Order.objects.none()
+        
+        queryset = Order.objects.filter(start_date__gte=start_date, embargo_date__lte=embargo_date)
+        return queryset
